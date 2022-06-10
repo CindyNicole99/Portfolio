2966efbfae2cb7c97f828594276e7e2f40670a22
CindyNicole99 <salcidocindy99@yahoo.com>
2022-06-10 08:42:45 -0500

    updated favicons

--- js/app.js
+++ js/app.js
@@ -7,3 +7,12 @@
 //         description:
 //     },
 // ]
+
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
